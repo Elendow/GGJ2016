998fadfd86ec874e3e1d5ff3e6313e4bd76155cb
Elendow <elendow@gmail.com>
2016-01-30 15:59:47 +0100

    Teleporter

---
+++ GGJ2016/Assets/Scripts/Utils/Teleporter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public Transform target;
+	
+	private void OnTriggerEnter2D(Collider2D other){
+		other.transform.position = target.position;
+	}
+}
