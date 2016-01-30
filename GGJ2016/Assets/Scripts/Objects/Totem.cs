1cb29951bcbac90cf8c18eb7dce0e9a5b5c10613
Elendow <elendow@gmail.com>
2016-01-30 21:58:16 +0100

    Escena gameplay guay con UI

--- GGJ2016/Assets/Scripts/Objects/Totem.cs
+++ GGJ2016/Assets/Scripts/Objects/Totem.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Totem : MonoBehaviour {
 
 	public int playerNum;
-	public SpriteRenderer[] ingredientsBackground;
-	public SpriteRenderer[] ingredientsIcon;
+	public Image[] ingredientsBackground;
+	public Image[] ingredientsIcon;
+	public Text scoreText;
 
+	private int _score = 0;
 	private Recipe _recipe;
 	private RecipeManager _recipeManager;
 
@@ -24,6 +27,8 @@ public class Totem : MonoBehaviour {
 		{
 			ingredientsIcon[i].sprite = _recipe.itemsSprites[i];
 		}
+
+		CalculateScore();
 	}
 
 	private void OnTriggerEnter2D(Collider2D other)
@@ -37,29 +42,54 @@ public class Totem : MonoBehaviour {
 				int _index = -1;
 				for(int i = 0; i < _recipe.itemsRecipe.Count; i++)
 				{
-					if(_recipe.itemsRecipe[i] == _i.itemID && _index == -1)
+					if(_recipe.itemsRecipe[i] == _i.itemID && _index == -1 && _recipe.itemsDone[i] == 0)
 						_index = i;
 				}
 
-				_recipe.itemsDone[_index] = 1;
-				ingredientsBackground[_index].color = Color.green;
+				if(_index != -1)
+				{
+					_recipe.itemsDone[_index] = 1;
+					ingredientsBackground[_index].color = Color.green;
+				}
+				else
+				{
+					BadItem();
+				}
 				other.gameObject.SetActive(false);
 			}
 			else
 			{
-				List<int> _missingItems = new List<int>();
-				for(int i = 0; i < _recipe.itemsRecipe.Count; i++)
-				{
-					if(_recipe.itemsDone[i] == 0)
-						_missingItems.Add(i);
-				}
+				BadItem();
 
-				int _screwItem = _missingItems[Random.Range(0, _missingItems.Count)];
-				_recipe.itemsDone[_screwItem] = -1;
-				ingredientsBackground[_screwItem].color = Color.red;
-				ingredientsIcon[_screwItem].sprite = null;
-				other.gameObject.SetActive(false);
 			}
+			CalculateScore();
+			other.gameObject.SetActive(false);	
 		}
+	}
+
+	private void BadItem()
+	{
+		List<int> _missingItems = new List<int>();
+		for(int i = 0; i < _recipe.itemsRecipe.Count; i++)
+		{
+			if(_recipe.itemsDone[i] == 0)
+				_missingItems.Add(i);
+		}
+
+		int _screwItem = _missingItems[Random.Range(0, _missingItems.Count)];
+		_recipe.itemsDone[_screwItem] = -1;
+		ingredientsBackground[_screwItem].color = Color.red;
+	}
+
+	private void CalculateScore()
+	{
+		_score = 0;
+
+	for(int i = 0; i < _recipe.itemsDone.Count; i++)
+		{
+			_score += _recipe.itemsDone[i];
+		}
+
+		scoreText.text = _score.ToString();
 	}
 }
