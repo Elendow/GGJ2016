2a983bce0bdec1b62e4bfa51eb7c04464fd0a011
Lucas González <lugonzalez@uma.es>
2016-01-30 23:49:49 +0100

    HE QUITADO EL TILE DE LAVA! ~~

---
+++ GGJ2016/Assets/Scripts/Tweens/AntorchaTween.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class AntorchaTween : MonoBehaviour {
+
+//	flo
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
