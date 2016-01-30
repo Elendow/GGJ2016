b7a531c3ea24aaa83a1b0e79fbe6a930bbcfafb9
Lucas González <lugonzalez@uma.es>
2016-01-31 00:17:37 +0100

    Iluminando 01

--- GGJ2016/Assets/Scripts/Tweens/AntorchaTween.cs
+++ GGJ2016/Assets/Scripts/Tweens/AntorchaTween.cs
@@ -4,15 +4,30 @@ using DG.Tweening;
 
 public class AntorchaTween : MonoBehaviour {
 
-//	flo
+	float delay;
+	float duration;
+	public float scalation = 2f;
+
+	public float minDelay;
+	public float maxDelay;
+
+	public float minDuration;
+	public float maxDuration;
+
+
 
 	// Use this for initialization
 	void Start () {
-	
+		delay = Random.Range (minDelay, maxDelay);
+		duration = Random.Range (minDuration, maxDuration);
+
+		transform.DOScale (transform.localScale.x *scalation, duration).SetDelay (delay).SetLoops (-1, LoopType.Yoyo).SetEase(Ease.InOutFlash);
+
+
+
+
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
