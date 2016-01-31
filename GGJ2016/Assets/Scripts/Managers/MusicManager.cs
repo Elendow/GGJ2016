dd28cffcbdf2f6e728526d723da54180452c9c06
Lucas González <lugonzalez@uma.es>
2016-01-31 02:15:54 +0100

    UI Prefab

---
+++ GGJ2016/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public GameObject soundPrefab;
+
+	public AudioClip clipItemCorrecto;
+	public AudioClip clipItemIncorrecto;
+	public AudioClip clipPickItem;
+	public AudioClip clipTiraItem;
+	public AudioClip clipRecogeItem;
+	public AudioClip clipCaeLava;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
